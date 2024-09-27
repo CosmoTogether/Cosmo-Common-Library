7954c7d4577cac1ff8daeb9b5d717b37d00b5258
Ahmad Riaz <ahmedriaz@leaddev.io>
2024-09-27 17:33:22 +0500

    Initial commit of CosmoAnimatorLibrary

---
+++ CosmoAnimator/src/main/java/com/leed/customanimator/cosmoAnimator.kt
@@ -0,0 +1,62 @@
+package com.leed.customanimator
+
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+
+class CosmoAnimator @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : RelativeLayout(context, attrs, defStyleAttr) {
+
+    private val maxOffset = 70f // Maximum offset in dp
+    private lateinit var imgProgress: ImageView
+    private lateinit var animator: ObjectAnimator
+    private lateinit var layout: RelativeLayout
+    private lateinit var msgText :TextView
+
+    init {
+        initView()
+    }
+
+    private fun initView() {
+        // Inflate the layout for the loader
+        LayoutInflater.from(context).inflate(R.layout.cosmo_animator, this, true)
+        layout = findViewById(R.id.checkProgress)
+        imgProgress = findViewById(R.id.imgProgress)
+        msgText = findViewById(R.id.msgText)
+    }
+
+    // Start the animation
+    fun startAnimation(message : String) {
+        layout.visibility = VISIBLE // Make the layout visible
+        msgText.text = message
+        val density = resources.displayMetrics.density
+        val maxOffsetPx = maxOffset * density  // Convert dp to pixels
+
+        // Initialize the ObjectAnimator
+        animator = ObjectAnimator.ofFloat(imgProgress, "translationX", 0f, maxOffsetPx).apply {
+            duration = 2000L  // Duration for smooth movement
+            interpolator = AccelerateDecelerateInterpolator()  // Smooth speed variation
+            repeatMode = ObjectAnimator.REVERSE
+            repeatCount = ObjectAnimator.INFINITE
+        }
+
+        // Start the animator
+        animator.start()
+    }
+
+    // Stop the animation
+    fun stopAnimation() {
+        if (this::animator.isInitialized) {
+            animator.cancel() // Cancel the animator
+        }
+        layout.visibility = GONE // Hide the layout
+    }
+}
